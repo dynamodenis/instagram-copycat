38dde5d28097aed23ae35b2d47ca88cfea84a46b
dynamodenis <dmbugua66@gmail.com>
2020-05-29 20:48:20 +0300

    comments model inside the image

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 from .models import Image,Comments
 # Register your models here.
+
+class CommentInline(admin.TabularInline):
+    model=Comments
+    extra=3
+    
+class ImageInline(admin.ModelAdmin):
+    fieldsets=[
+        (None,{'fields':['image']}),
+        (None,{'fields':['image_name']}),
+        (None,{'fields':['image_caption']}),
+        (None,{'fields':['likes']}),
+    ]
+    inlines=[CommentInline]
+    
+    
 admin.site.site_header='InstaPost Admin'
 admin.site.site_title='InstaPost Admin Dashboard'
 
-admin.site.register(Image)
-admin.site.register(Comments)
+admin.site.register(Image,ImageInline)
+# admin.site.register(Comments)
