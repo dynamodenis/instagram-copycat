e017e5be500ca71ed3e003e48ed3650959767644
dynamodenis <dmbugua66@gmail.com>
2020-05-29 13:02:36 +0300

    created image model and comments model

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image=models.ImageField(upload_to='images/', blank=True,default='default.jpg')
+    image_name=models.CharField(max_length=30)
+    image_caption=models.TextField()
+    likes=models.ImageField(default=0)
+    
+    def __str__(self):
+        return 'self.image_name'
+    
+class Comments(models.Model):
+    image=models.ForeignKey(Image, on_delete=models.CASCADE)
+    comment=models.TextField()
+    likes=models.IntegerField(default=0)
+    
+    def __str__(self):
+        return 'self.comment'
+    
