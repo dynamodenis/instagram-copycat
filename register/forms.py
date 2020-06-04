3c8002f7f6758a85107e1b187b3a0d44004aa798
dynamodenis <dmbugua66@gmail.com>
2020-06-04 14:46:57 +0300

    register page modified all helper texts hidden

--- register/forms.py
+++ register/forms.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class RegisterUser(UserCreationForm):
+    def __init__(self,*args,**kwargs):
+        super(RegisterUser,self).__init__(*args,**kwargs)
+        for fieldname in ['username','email','password1','password2']:
+            self.fields[fieldname].help_text=None
+            
     email=forms.EmailField()
     
     class Meta:
